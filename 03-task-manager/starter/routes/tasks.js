c9bf1ec61bca477b85b2d380b6113acf2ad7c76b
Bruno Medice Mariano <brunomedice@gmail.com>
2021-07-23 14:01:13 +0800

    add routes

---
+++ 03-task-manager/starter/routes/tasks.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const { getAllTask, createTask, getSingleTask, updateTask, deleteTask } = require('../controllers/tasks')
+
+router.route('/').get(getAllTask).post(createTask)
+router.route('/:id').get(getSingleTask).patch(updateTask).delete(deleteTask)
+
+
+module.exports = router
