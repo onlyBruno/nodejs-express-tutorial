2c198b666b1e076e8853b1836edd4dbb59858e2f
Bruno Medice Mariano <brunomedice@gmail.com>
2021-08-09 23:02:38 +0800

    update user model

--- 06-jobs-api/starter/models/User.js
+++ 06-jobs-api/starter/models/User.js
@@ -1,4 +1,6 @@
 const moongose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const UserSchema = new moongose.Schema({
   name: {
@@ -23,5 +25,24 @@ const UserSchema = new moongose.Schema({
   },
 })
 
-module.exports = moongose.model('User', UserSchema)
+UserSchema.pre('save', async function () {
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+})
 
+UserSchema.methods.createJWT = function () {
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_LIFETIME,
+    }
+  )
+}
+
+UserSchema.methods.verifyPasswordLogin = async function (canditatePassword) {
+  const isMatch = await bcrypt.compare(canditatePassword, this.password)
+  return isMatch
+}
+
+module.exports = moongose.model('User', UserSchema)
