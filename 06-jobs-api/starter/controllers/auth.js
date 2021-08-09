01b4e5493ba6a0894765224381d4b4da36419cc9
Bruno Medice Mariano <brunomedice@gmail.com>
2021-08-09 23:00:07 +0800

    add authentication to jobs route

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,26 +1,30 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
-const { BadRequestError } = require('../errors')
-const bcrypt = require('bcryptjs')
-
-
+const { BadRequestError, UnauthenticatedError } = require('../errors')
 
 const register = async (req, res) => {
-const {name, email, password} = req.body
-
-//  generate random byte, more rounds we have, more cpu usage we will have
-const salt = await bcrypt.genSalt(10)
-const hashedPassword = await bcrypt.hash(password, salt)
-
-const tempUser = {name,email,password:hashedPassword}
-
-
-  const user = await User.create({ ...tempUser })
-  res.status(StatusCodes.CREATED).json({ user })
+  const user = await User.create({ ...req.body })
+  const token = user.createJWT()
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
 }
 
 const login = async (req, res) => {
-  res.send('login user')
+  const { email, password } = req.body
+  if (!email || !password) {
+    throw new BadRequestError('Please provide email and password')
+  }
+  const user = await User.findOne({ email })
+  if (!user) {
+    throw new UnauthenticatedError('email/password are incorrect')
+  }
+  //  checking password
+  const isPasswordCorrect = await user.verifyPasswordLogin(password)
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError('email/password are incorrect')
+  }
+
+  const token = user.createJWT()
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token })
 }
 
 module.exports = {
