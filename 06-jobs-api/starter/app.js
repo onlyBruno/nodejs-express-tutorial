eaad546718b7d4d4753011897b58df213fccc26d
Bruno Medice Mariano <brunomedice@gmail.com>
2021-08-10 02:11:14 +0800

    add security lib

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -1,5 +1,12 @@
 require('dotenv').config()
 require('express-async-errors')
+
+//  extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
 const express = require('express')
 const app = express()
 
@@ -18,8 +25,14 @@ const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
 app.use(express.json())
-// extra packages
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+app.use(rateLimiter({ windowMs: 60 * 1000, max: 60 }))
 
+app.get('/', (req,res) => {
+  res.send('jobs api')
+})
 // routes
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
