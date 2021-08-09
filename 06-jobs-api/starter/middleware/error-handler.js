fcfe972fcff9d2af8069bc9cc12bc037aa591f98
Bruno Medice Mariano <brunomedice@gmail.com>
2021-08-10 01:03:37 +0800

    add validation custom error

--- 06-jobs-api/starter/middleware/error-handler.js
+++ 06-jobs-api/starter/middleware/error-handler.js
@@ -3,15 +3,25 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   let customError = {
     //  set default
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
-    msg: err.message || 'Something wentg wrong, please try again later'
+    msg: err.message || 'Something wentg wrong, please try again later',
   }
-  if(err.code && err.code === 11000){
-    customError.msg = `This ${Object.keys(err.keyValue)} address is already being used`
+  if (err.code && err.code === 11000) {
+    customError.msg = `This ${Object.keys(
+      err.keyValue
+    )} address is already being used`
     customError.statusCode = 400
   }
-  if(err.name === 'CastError'){
+  if (err.name === 'CastError') {
     customError.msg = `No item found with id: ${err.value}`
     customError.statusCode = 404
+  }
+  if (err.name === 'ValidationError') {
+    customError.msg =
+      'Please provide ' +
+      Object.values(err.errors)
+        .map((item) => item.message.split(' ')[2])
+        .join(', ')
+    customError.statusCode = 400
   }
   return res.status(customError.statusCode).json({ msg: customError.msg })
 }
