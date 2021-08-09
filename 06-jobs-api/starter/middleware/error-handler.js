9eea655b971e2163d61cb84be94601fbf592bd28
Bruno Medice Mariano <brunomedice@gmail.com>
2021-08-10 00:18:41 +0800

    add custom err msg castError and Duplicated mail

--- 06-jobs-api/starter/middleware/error-handler.js
+++ 06-jobs-api/starter/middleware/error-handler.js
@@ -1,10 +1,19 @@
-const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+  let customError = {
+    //  set default
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something wentg wrong, please try again later'
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  if(err.code && err.code === 11000){
+    customError.msg = `This ${Object.keys(err.keyValue)} address is already being used`
+    customError.statusCode = 400
+  }
+  if(err.name === 'CastError'){
+    customError.msg = `No item found with id: ${err.value}`
+    customError.statusCode = 404
+  }
+  return res.status(customError.statusCode).json({ msg: customError.msg })
 }
 
 module.exports = errorHandlerMiddleware
